4b6f7881c35142ac60704e2e64d32b8649b6761e
Konstantin Neureither <konstantin.neureither@cdtm.de>
2025-02-27 01:57:26 +0100

    multiline formatting

--- Tests/UITests/TestApp/TestAppDelegate.swift
+++ Tests/UITests/TestApp/TestAppDelegate.swift
@@ -17,9 +17,11 @@ class TestAppDelegate: SpeziAppDelegate {
             AccessGuardModule(
                 [
                     .biometrics(identifier: "TestBiometricsIdentifier"),
-                    .code(identifier: "TestIdentifier",
-                          codeOptions: [.all],
-                          timeout: 10),
+                    .code(
+                        identifier: "TestIdentifier",
+                        codeOptions: [.all],
+                        timeout: 10
+                    ),
                     .fixed(identifier: "TestFixedIdentifier", code: "1234")
                 ]
             )
